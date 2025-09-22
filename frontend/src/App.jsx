2d65dbd462e316659b964d110038c4310ee20f29
Binh Tran <binh412005@gmail.com>
2025-09-22 00:49:43 -0400

    set up fe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,6 @@
-import { useState } from 'react'
-import Container from '@mui/material/Container'
-import AppBar from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
-import Box from '@mui/material/Box'
-import './App.css'
+import {Button} from '@mui/material'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            LMS Project
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-
-      <Container sx={{ mt: 4 }}>
-        <Box sx={{ textAlign: 'center' }}>
-          <Typography variant="h4" gutterBottom>
-            Welcome to the LMS
-          </Typography>
-
-          <Button variant="contained" color="primary" onClick={() => setCount(c => c + 1)}>
-            Clicked {count} times
-          </Button>
-        </Box>
-      </Container>
-    </>
-  )
+const App = () => {
+  return <Button>Click me</Button>
 }
-
-export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ThemeProvider } from '@mui/material/styles'
-import CssBaseline from '@mui/material/CssBaseline'
-import '@fontsource/roboto'
-import './index.css'
-import App from './App.jsx'
-import theme from './theme'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
       <App />
-    </ThemeProvider>
   </StrictMode>,
 )
